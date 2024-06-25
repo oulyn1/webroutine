36de1e91611d8fc86d535ae7a33a0b98d5d752fd
minhieu204 <minhhieu13062004@gmail.com>
2024-06-25 22:04:53 +0700

    hi

--- web/webforadmin/donhang/donhang.php
+++ web/webforadmin/donhang/donhang.php
@@ -33,6 +33,48 @@
             <div class="body">
                 <div class="body-headersanpham">
                     <h1>Quản lý đơn hàng</h1>
+                    <div class="tbl_hienthi">
+                    <?php
+                        include("../../../config/config.php");
+                        //buoc 2 viet truy van
+                        $query = "SELECT * FROM donhang,user 
+                        WHERE user.id_user=donhang.iduser ORDER BY iddonhang DESC";
+                        //buoc 3 thuc thi cau lenh
+                        $result = mysqli_query($conn, $query);
+                        //buoc 4 lay du lieu
+                        if(mysqli_num_rows($result) >0){
+                            echo '<table id="tblMain">
+                                    <thead>
+                                        <th>ID đơn hàng</th>
+                                        <th>Tên khách hàng</th>
+                                        <th>Ngày lập đơn</th>
+                                        <th>Địa chỉ</th>
+                                        <th>Tổng tiền</th>
+                                        <th>Tình trạng</th>
+                                        <th>Sửa</th>
+                                        <th>Xóa</th>
+                                    </thead>
+                                    <tbody>';
+                            while ($row = mysqli_fetch_assoc($result)){
+                                echo'<tr>
+                                        <td>'.$row["iddonhang"].'</td>
+                                        <td>'.$row["fullname"].'</td>
+                                        <td>'.$row["ngaylapdon"].'</td>
+                                        <td>'.$row["address"].'</td>
+                                        <td>'.$row["tongtien"].'</td>
+                                        <td>'.$row["tinhtrang"].'</td>
+                                        <td><a class="thea" href="suadanhmuc.php?ID='.$row["iddonhang"].'"><i class="ti-pencil-alt"></i></a></td>
+                                        <td><a class="thea" onclick="return confirm(\'Bạn có muốn xóa danh mục không?\');" href="xoadanhmuc.php?ID='.$row["iddanhmuccon"].'"><i class="ti-trash"></i></a></td>
+                                    </tr>';
+                                    }
+                    
+                            echo'  </tbody>
+                                </table>';
+                            
+                        }else{
+                            echo 'Không có dữ liệu sản phẩm';
+                        }
+                    ?>
                 </div>
             </div>
         </div>
