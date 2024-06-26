efb292e86e45ab17765b3053a6ff8ec8d32c5f43
Oulyne <minhkhoa1401@gmail.com>
2024-06-26 23:02:03 +0700

    sua tiep

--- web/webforadmin/khachhang/create_customer.php
+++ web/webforadmin/khachhang/create_customer.php
@@ -23,11 +23,7 @@
     <div class="container">
         <div class="header">
             <div class="header__list">
-<<<<<<< HEAD
             <a href="#" class="header__list-items" ><b>Tổng quan</b></a>
-=======
-                <a href="#" class="header__list-items" id="main"><b>Tổng quan</b></a>
->>>>>>> 8c3f54a65aafe3ac3e660cd6c905e67978483bbf
                 <a href="../khachhang/khachhang.php?ID=ID" class="header__list-items"><b>Khách hàng</b></a>
                 <a href="../taikhoan/taikhoan.php?ID=ID" class="header__list-items"id="main"><b>Tài khoản</b></a>
                 <a href="../sanpham/sanpham.php?ID=ID" class="header__list-items"><b>Sản phẩm</b></a>
