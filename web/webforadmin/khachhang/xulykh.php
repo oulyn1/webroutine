8c3f54a65aafe3ac3e660cd6c905e67978483bbf
ttoanhb24 <ncthb24@gmaail.com>
2024-06-26 22:56:59 +0700

    123

--- web/webforadmin/khachhang/xulykh.php
+++ web/webforadmin/khachhang/xulykh.php
@@ -12,29 +12,27 @@ if ($timkiem != "") {
 }
 
 $result = mysqli_query($conn, $sql);
-$num = 1;
 if (mysqli_num_rows($result) > 0) {
     echo '<table id="tblMain">
         <thead>
-            <th>ID</th>
-            <th>Tên KH</th>
-            <th>Email</th>
-            <th>Số điện thoại</th>
-            <th>Địa chỉ</th>
-            <th>Ngày tạo</th>
-            <th>Cập Nhật</th>
+            <tr>
+                <th>ID</th>
+                <th>Tên KH</th>
+                <th>Email</th>
+                <th>Số điện thoại</th>
+                <th>Địa chỉ</th>
+                <th>Cập Nhật</th>
+            </tr>
         </thead>
-    
     <tbody>';
     while($row = mysqli_fetch_assoc($result)) {
         echo '<tr>
-        <td>' . $row["id"] . '</td>
-        <td>' . $row["ten"] . '</td>
-        <td>' . $row["email"] . '</td>
-        <td>' . $row["so_dien_thoai"] . '</td>
-        <td>' . $row["dia_chi"] . '</td>
-        <td>' . $row["ngay_tao"] . '</td>
-        <td><a href="editkh.php?id=' . $row["id"] . '">Sửa</a> | <a href="delete_customer.php?id=' . $row["id"] . '">Xóa</a></td>
+            <td>' . $row["id"] . '</td>
+            <td>' . $row["ten"] . '</td>
+            <td>' . $row["email"] . '</td>
+            <td>' . $row["so_dien_thoai"] . '</td>
+            <td>' . $row["dia_chi"] . '</td>
+            <td><a href="editkh.php?id=' . $row["id"] . '">Sửa</a> | <a href="delete_customer.php?id=' . $row["id"] . '">Xóa</a></td>
         </tr>';
     }
     echo '</tbody> 
