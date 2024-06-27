7b529769a112d338b831453ae36f43b3d5c45dff
ttoanhb24 <ncthb24@gmaail.com>
2024-06-27 23:52:59 +0700

    123

--- web/webforcustomer/cua_hangcst.php
+++ web/webforcustomer/cua_hangcst.php
@@ -3,72 +3,130 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Thanh toán</title>
-    <link rel="stylesheet" href="../../asset/css/thanhtoan.css">
-    <link rel="stylesheet" href="../../asset/css/footer.css">
+    <title>Quần áo thời trang nam</title>
+    <link rel="stylesheet" href="../../asset/css/customer.css?v= <?php echo time(); ?>">
     <link rel="stylesheet" href="../../asset/css/themify-icons-font/themify-icons/themify-icons.css">
+    <link rel="stylesheet" href="../../asset/css/footer.css">
+    <link rel="stylesheet" href="../../asset/css/thanhtoan.css">
 </head>
 <body>
-<div class="main">
-
-    <div id="header">
-
+  <div id="main">
+      <div id="header">
         <div class="header__left">
-
-            <a href="" class="Logo">
-
-                <img src="../../asset/img/logo-routine.png" alt=""  class="Logo__img">
-            </a>
-
-            <ul class="nav">
-
-                <li><a href="">Nam</a></li>
-                <li><a href="">Nữ</a></li>
-                <li><a href="">New</a></li>
-                <li><a href="">Best</a></li>
+          <a href="mainafterlogin.php" class="logo">
+            <img
+              src="../../asset/img/logo-routine.png"
+              alt=""
+              class="logo__img"
+            />
+          </a>
+          <ul id="nav">
+          <li>
+                    <a href="manafterlogin.php">NAM</a>
+                    <?php
+                    include("../../config/config.php");
+                    //buoc 2 viet truy van
+                    $query = "SELECT * FROM tbl_danhmuccon";
+                    //buoc 3 thuc thi cau lenh
+                    $result = mysqli_query($conn, $query);
+                    //buoc 4 lay du lieu
+                    if(mysqli_num_rows($result) >0){
+                      echo'<ul class="subnav">';
+                      while ($row = mysqli_fetch_assoc($result)){
+                        if($row["idloaisanpham"]==1){
+                          echo'<li><a href="../webforcustomer/aonamafterlogin.php?IDDM='.$row["iddanhmuccon"].'&IDLSP='.$row["idloaisanpham"].'">'.$row["tendanhmuccon"].'</a></li>';
+                        }
+                      }
+                      echo'</ul>';
+                    }
+                    ?>
+                </li>
+                <li>
+                    <a href="womenafterlogin.php">NỮ</a>
+                    <?php
+                    include("../../config/config.php");
+                    //buoc 2 viet truy van
+                    $query = "SELECT * FROM tbl_danhmuccon";
+                    //buoc 3 thuc thi cau lenh
+                    $result = mysqli_query($conn, $query);
+                    //buoc 4 lay du lieu
+                    if(mysqli_num_rows($result) >0){
+                      echo'<ul class="subnav">';
+                      while ($row = mysqli_fetch_assoc($result)){
+                        if($row["idloaisanpham"]==2){
+                          echo'<li><a href="../webforcustomer/aonamafterlogin.php?IDDM='.$row["iddanhmuccon"].'&IDLSP='.$row["idloaisanpham"].'">'.$row["tendanhmuccon"].'</a></li>';
+                        }
+                      }
+                      echo'</ul>';
+                    }
+                    ?>
+                </li>
             </ul>
-
         </div>
-
         <div class="header__right">
-
-            <a href="#" class="search">
-                <i class="ti-search"></i>
-                <span class="search__title">Tìm kiếm</span>
-            </a>
-            <a class="ti-user" href=""></a>
-            <a class="ti-heart" href=""></a>
-
+          <a class="search" href="">
+            <i class="ti-search"></i>
+            <span class="search__title">Tìm kiếm</span>
+          </a>
+          <span class="ti-user" id="openlogin" href=""></span>
+          <span class="ti-heart" id="openlove" href=""></span>
+          <span class="ti-shopping-cart" id="openshopping" href=""></span>
         </div>
+      </div>
 
+
+      <div class="model" id="modal-love">
+      <div class="model-overlay"></div>
+      <div class="modal-right">
+        <div class="modal-love">
+          <div class="love-heading">
+            <p>
+              <i class="ti-heart"></i>
+              <b>Sản phẩm yêu thích</b>
+            </p>
+            <span class="ti-close"></span>
+          </div>
+        </div>
+      </div>
     </div>
 
-    <!-- content -->
-
-    <div class="content">
-
-        <div class="pice">
-
-            <ul>
-                <li><a href="">Trang chủ</a></li>
-                <li> / Giỏ hàng</li>
-            </ul>
-
+    <div class="model" id="modal-shopping">
+      <div class="model-overlay"></div>
+      <div class="modal-right">
+        <div class="modal-love">
+          <div class="love-heading">
+            <p>
+              <i class="ti-shopping-cart"></i>
+              <b>Giỏ hàng</b>
+            </p>
+            <span class="ti-close"></span>
+          </div>
         </div>
-
-        <div class="check">
-
-            <div class="check-img"></div>
-            <h1>Giỏ hàng</h1>
-
-        </div>
-
-        <div class="content-main">
-
-        </div>
+      </div>
     </div>
 
-    <!-- content -->
+    <div class="model" id="modal-signin">
+      <div class="model-overlay"></div>
+      <div class="modal-after">
+        <div class="model-innerafter">
+          <span class="ti-close"></span>
+        </div>
+        <div class="body-start">Chào khách hàng</div>
+        <hr />
+        <div class="body-after">
+          <div>
+            <a href=""><p>Theo dõi đơn hàng</p></a>
+          </div>
+          <div>
+            <a href=""><p>Sản phẩm yêu thích</p></a>
+          </div>
+        </div>
+        <hr />
+        <div class="body-end"><a href="../webforadmin/logout.php">Đăng xuất</a></div>
+      </div>
+    </div>
+
+    <script src="../../asset/js/mainafterlogin.js"></script>
 
     <div class="content">
         <div class="content-main">
@@ -91,83 +149,77 @@
         </div>
     </div>
 
-    <!-- footer -->
+<div class="footer">
 
-    <div class="footer">
+<div class="information">
 
-        <div class="information">
+    <img src="../../asset/img/logo-routine.png" alt="" class="logo">
+    <h3>CÔNG TY TNHH ROUTINE VIETNAM</h3>
+    <P>Mã số thuế: 0106486365</P>
+    <P></P>
+    <P>Văn phòng: tầng 5 tòa nhà IMC, 62 Trần Quang Khải - Phường Tân Định - Quận 1 - TP Hồ Chí Minh.</P>
+    <h3>THAM GIA BẢNG TIN CỦA CHÚNG TÔI</h3>
 
-            <img src="../../asset/img/logo-routine.png" alt="" class="logo">
-            <h3>CÔNG TY TNHH ROUNTIINE VIETNAM</h3>
-            <P>Mã số thuế: 0106486365</P>
-            <P></P>
-            <P>Văn phòng: tầng 5 tòa nahf IMC, 62 Trần Quang Khải - Phường Tân Định - Quận 1 - TP Hồ Chí Minh.</P>
-            <h3>THAM GIA BẢNG TIN CỦA CHÚNG TÔI</h3>
+</div>
 
-        </div>
+<div class="about">
 
-        <div class="about">
+    <div class="company">
 
-            <div class="company">
+        <h5>CÔNG TY</h5>
+        <ul>
+            <li><a href="">Giới thiệu về ROUTINE</a></li>
+            <li><a href="">THE 31</a></li>
+            <li><a href="">Tuyển dụng</a></li>
+            <li><a href="">Tin thời trang</a></li>
+            <li><a href="">Hợp tác nhượng quyền</a></li>
+            <li><a href="">Liên hệ</a></li>
+        </ul>
 
-                <h5>CÔNG TY</h5>
-                <ul>
-                    <li><a href="">Giới thiệu về ROUTINE</a></li>
-                    <li><a href="">THE 31</a></li>
-                    <li><a href="">Tuyển dụng</a></li>
-                    <li><a href="">Tin thời trang</a></li>
-                    <li><a href="">Hợp tác nhượng quyền</a></li>
-                    <li><a href="">Liên hệ</a></li>
-                </ul>
-
-            </div>
-
-            <div class="social">
-
-                <h3>KẾT NỐI VỚI CHÚNG TÔI</h3>
-
-            </div>
-
-        </div>
-
-        <div class="policy">
-
-            <h5>CHÍNH SÁCH KHÁCH HÀNG</h5>
-            <ul>
-                <li><a href="">Chính sách khách hàng thân thiết</a></li>
-                <li><a href="">Chính sách đổi trả</a></li>
-                <li><a href="">Chính sách bảo hành</a></li>
-                <li><a href="">Chính sách bảo mật</a></li>
-                <li><a href=""> Câu hỏi thường gặp</a></li>
-                <li><a href="">hướng dẫn mua hàng online</a></li>
-                <li><a href="">Hướng dẫn kiểm trra hạng thành viên</a></li>
-            </ul>
-            
-        </div>
-
-        <div class="store">
-
-            <h5>THÔNG TIN CỬA HÀNG</h5>
-            <ul>
-                <li>
-                    <h4>CỬA HÀNG THỨU 34</h4>
-                    <p>F15 tầng 1 AEON Mall Tân Phú, 30 Bờ Bao Tân Thắng, Phường Sơn Kỳ, TP Hồ Chí Minh</p>
-                </li>
-                <li>
-                    <h4>CỬA HÀNG THỨ 33</h4>
-                    <p>809 Giải Phóng, Phường Giáp Bát, Quận Hoàng Mai, TP Hà Nội</p>
-                </li>
-                <li>
-                    <h4>CỬA HÀNG THỨ 32</h4>
-                    <p>192 - 194 Hoa Lan, Phường 2, Quận Phú Nhuận, TP Hồ Chí Minh</p>
-                </li>
-            </ul>
-            <a href="">XEM TẤT CẢ CỬA HÀNG</a>
-        </div>
     </div>
+
+    <div class="social">
+
+        <h3>KẾT NỐI VỚI CHÚNG TÔI</h3>
+
+    </div>
+
+</div>
+
+<div class="policy">
+
+    <h5>CHÍNH SÁCH KHÁCH HÀNG</h5>
+    <ul>
+        <li><a href="">Chính sách khách hàng thân thiết</a></li>
+        <li><a href="">Chính sách đổi trả</a></li>
+        <li><a href="">Chính sách bảo hành</a></li>
+        <li><a href="">Chính sách bảo mật</a></li>
+        <li><a href=""> Câu hỏi thường gặp</a></li>
+        <li><a href="">hướng dẫn mua hàng online</a></li>
+        <li><a href="">Hướng dẫn kiểm trra hạng thành viên</a></li>
+    </ul>
+    
+</div>
+
+<div class="store">
+
+    <h5>THÔNG TIN CỬA HÀNG</h5>
+    <ul>
+        <li>
+            <h4>CỬA HÀNG THỨU 34</h4>
+            <p>F15 tầng 1 AEON Mall Tân Phú, 30 Bờ Bao Tân Thắng, Phường Sơn Kỳ, TP Hồ Chí Minh</p>
+        </li>
+        <li>
+            <h4>CỬA HÀNG THỨ 33</h4>
+            <p>809 Giải Phóng, Phường Giáp Bát, Quận Hoàng Mai, TP Hà Nội</p>
+        </li>
+        <li>
+            <h4>CỬA HÀNG THỨ 32</h4>
+            <p>192 - 194 Hoa Lan, Phường 2, Quận Phú Nhuận, TP Hồ Chí Minh</p>
+        </li>
+    </ul>
+    <a href="cua_hangcst.php">XEM TẤT CẢ CỬA HÀNG</a>
+</div>
 </div>
 </body>
 </html>
-<?php
-$conn->close();
-?>
