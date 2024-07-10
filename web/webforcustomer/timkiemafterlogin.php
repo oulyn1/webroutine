c4a503cedca0fdcf569ca40d736d8c8f62243f4a
minhieu204 <minhhieu13062004@gmail.com>
2024-07-10 09:24:26 +0700

    hi

--- web/webforcustomer/timkiemafterlogin.php
+++ web/webforcustomer/timkiemafterlogin.php
@@ -18,6 +18,7 @@ if (!isset($_SESSION["customer"])) {
     <link rel="stylesheet" href="../../asset/css/themify-icons-font/themify-icons/themify-icons.css">
     <link rel="stylesheet" href="../../asset/css/footer.css">
     <link rel="stylesheet" href="../../asset/css/timkiem.css">
+    <link rel="stylesheet" href="../../asset/css/infoItemStyle.css?v= <?php echo time(); ?>">
     <link rel="shortcut icon" href="../../asset/img/favicon.ico" type="image/x-icon">
 </head>
 <body>
